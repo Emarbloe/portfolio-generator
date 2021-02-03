13e65a5a98f5cd4525cb6b8326d2cd0c34ab1b8b
Emarbloe <72406130+Emarbloe@users.noreply.github.com>
2021-02-03 05:24:42 -0600

    add intial node files

---
+++ app.js
@@ -0,0 +1,13 @@
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    //This
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+
+    console.log('==============');
+
+    // Is the same as this
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+  };
